3ce4c410f84180d9136fa926679e0d547a72274b
jonathanmutal <jonathanmutal95@gmail.com>
2017-04-07 14:36:39 -0300

    Ejercicio1 y Ejercicio2 finished

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,6 +1,6 @@
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
-
+from math import log
 
 class NGram(object):
 
@@ -14,16 +14,73 @@ class NGram(object):
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
+            sent += ['</s>']
+            if n > 1:
+                sent = ['<s>'] + sent
             for i in range(len(sent) - n + 1):
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
 
-    def prob(self, token, prev_tokens=None):
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return self.counts.get(tokens)
+
+    def cond_prob(self, token, prev_tokens=None):
+
         n = self.n
         if not prev_tokens:
             prev_tokens = []
         assert len(prev_tokens) == n - 1
 
         tokens = prev_tokens + [token]
-        return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+
+        count_prev_tokens = self.counts[tuple(prev_tokens)]
+        if count_prev_tokens == 0:
+            prob = 0
+        else:
+            prob = float(self.counts[tuple(tokens)] / count_prev_tokens)
+        return prob
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+        sent -- the sentence as a list of tokens.
+        """
+        n = self.n
+
+        sent += ['</s>']
+
+        count_label = n
+        while count_label > 1:
+            sent = ['<s>'] + sent
+            count_label -= 1
+
+        prob = 1
+        for index_word in range(n - 1, len(sent)):
+            prob *= self.cond_prob(sent[index_word], sent[index_word-n+1: index_word])
+
+        return prob
+
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+        sent -- the sentence as a list of tokens.
+        """
+        n = self.n
+        log2 = lambda x: log(x, 2) if x > 0 else float('-inf')
+
+        sent += ['</s>']
+
+        count_label = n
+        while count_label > 1:
+            sent = ['<s>'] + sent
+            count_label -= 1
+
+        prob = 0
+        for index_word in range(n - 1, len(sent)):
+            prob += log2(self.cond_prob(sent[index_word], sent[index_word-n+1: index_word]))
+
+        return prob
+
--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,34 +1,59 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py [-n <n>] [-o <file>]
   train.py -h | --help
 
 Options:
-  -n <n>        Order of the model.
-  -o <file>     Output model file.
+  -n <n>        Order of the model [default: None].
+  -o <file>     Output model file [default: ejemplo.txt].
   -h --help     Show this screen.
 """
+
+DEFAULT_DIR = 'test'
+
 from docopt import docopt
 import pickle
 
 from nltk.corpus import gutenberg
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import RegexpTokenizer
 
 from languagemodeling.ngram import NGram
 
+def gutenberg(arg):
+    n = int(opts['-n'])
+    filename = opts['-o']
+
+    sents = gutenberg.sents('austen-emma.txt')
+    # train the model
+    model = NGram(n, sents)
+
+    # save it
+    f = open(filename, 'wb')
+    pickle.dump(model, f)
+    print ("Se guardara en", filename)
+    f.close()
+
+def my_token(filename, directory=DEFAULT_DIR):
+    corpus = PlaintextCorpusReader(directory, filename)
+    pattern = r'''(?ix)    # set flag to allow verbose regexps
+            (?:sr\.|sra\.)
+            | (?:[A-Z]\.)+        # abbreviations, e.g. U.S.A.
+            | \w+(?:-\w+)*        # words with optional internal hyphens
+            | \$?\d+(?:\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+            | \.\.\.            # ellipsis
+            | [][.,;"'?():-_`]
+            '''
+    tokenizer = RegexpTokenizer(pattern)
+    corpus = PlaintextCorpusReader(DEFAULT_DIR, 'ejemplo.txt', word_tokenizer=tokenizer)
+    return corpus.sents()
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    if opts['-n'] is None:
+        gutenberg(opts)
+    else:
+        print (my_token(opts['-o']))
 
-    # train the model
-    n = int(opts['-n'])
-    model = NGram(n, sents)
-
-    # save it
-    filename = opts['-o']
-    f = open(filename, 'wb')
-    pickle.dump(model, f)
-    f.close()
