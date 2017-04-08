432a805c9abc1e52193e4c6143dfc410e04549ba
jonathanmutal <jonathanmutal95@gmail.com>
2017-04-08 12:26:34 -0300

    exercise 3 began. add some comments. In NgramGenerator generate_token and init have already finished.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,6 +1,9 @@
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
+
 from math import log
+
+from random import random
 
 class NGram(object):
 
@@ -12,6 +15,8 @@ class NGram(object):
         assert n > 0
         self.n = n
         self.counts = counts = defaultdict(int)
+        # to save only the (n-1)-gram tuple.
+        self.tokens = []
 
         for sent in sents:
             sent += ['</s>']
@@ -20,6 +25,8 @@ class NGram(object):
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
+                if len(ngram) == n:
+                    self.tokens += [ngram]
 
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
@@ -55,6 +62,8 @@ class NGram(object):
 
         prob = 1
         for index_word in range(n - 1, len(sent)):
+            # the probability of P(word.index_word|word.index_word-1,..., word.(index_word - (n-1)))
+            # in a n-gram model.
             prob *= self.cond_prob(sent[index_word], sent[index_word-n+1: index_word])
 
         return prob
@@ -64,6 +73,7 @@ class NGram(object):
         sent -- the sentence as a list of tokens.
         """
         n = self.n
+        # log2 domain extended. Domain = N U 0.
         log2 = lambda x: log(x, 2) if x > 0 else float('-inf')
 
         sent += ['</s>']
@@ -74,3 +84,71 @@ class NGram(object):
             prob += log2(self.cond_prob(sent[index_word], sent[index_word-n+1: index_word]))
 
         return prob
+
+class NGramGenerator:
+
+    def __init__(self, model):
+        """
+        model -- n-gram model.
+        """
+        self.model = model
+        self.probs = probs = defaultdict(dict)
+        self.sorted_probs = sorted_probs = defaultdict()
+
+        list_tokens = self.model.tokens
+        n = self.model.n
+        cond_prob = self.model.cond_prob
+
+        for token in list_tokens:
+            word = token[n-1]
+            tokens = token[0:n-1]
+            # conditional probability of word given the n-1 tokens
+            prob_conditional = cond_prob(word, list(tokens))
+            probs[tokens][word] = prob_conditional
+            # be carefull. ask about this!
+            # sorted_probs is for inverse transform sampling.
+            sorted_probs[tokens] = sorted(probs[tokens].items())
+
+
+    def generate_sent(self):
+        """Randomly generate a sentence."""
+
+
+
+    def generate_token(self, prev_tokens=None):
+        """Randomly generate a token, given prev_tokens.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        n = self.model.n
+
+        if not prev_tokens:
+            prev_tokens = []
+        assert len(prev_tokens) == n - 1
+
+        # a copy of self.sorted_probs[prev_tokens]
+        sorted_probs_tokens = list(self.sorted_probs[prev_tokens])
+        # uniform variable
+        U = random()
+        # p will represent the p0 + ... + pi probabilities
+        p = 0
+        next_token = ''
+        # inverse transform sampling
+        while U > p:
+            next_token, pi = sorted_probs_tokens.pop(0)
+            p += pi
+
+        return next_token
+
+
+
+
+
+sents = [
+    'el gato come pescado .'.split(),
+    'la gata come salmón .'.split(),
+]
+
+
+ngram = NGram(2, sents)
+generator = NGramGenerator(ngram)
+print (generator.sorted_probs)
