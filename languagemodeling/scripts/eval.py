3c16fda6d87cd6c29762aa0be631f410dea3cfae
jonathanmutal <jonathanmutal95@gmail.com>
2017-04-13 09:35:55 -0300

    adding eval.py

---
+++ languagemodeling/scripts/eval.py
@@ -0,0 +1,11 @@
+"""
+Evaulate a language model using the test set.
+
+Usage:
+  eval.py -i <file>
+  eval.py -h | --help
+
+Options:
+  -i <file>     Language model file.
+  -h --help     Show this screen.
+"""
