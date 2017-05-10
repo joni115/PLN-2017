1c10f06fe146769d84da7035109a736a8ed1b190
jonathanmutal <jonathanmutal95@gmail.com>
2017-05-10 11:25:24 -0300

    baseline finished

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,4 +1,4 @@
-
+from collections import Counter, defaultdict
 
 class BaselineTagger:
 
@@ -6,7 +6,21 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+
+        words_tagged = [word_tagg for sent in tagged_sents for word_tagg in sent]
+        words_tagged_count = Counter(words_tagged)
+
+        # best_tag will save the best tag of the word.
+        # example {(word1, tagg1): 10, (word2, tagg20): 30,...}
+        best_tag = defaultdict(int)
+        # to save the best tagg for a word
+        taggs_for_words = defaultdict(str)
+        for word, tagg in words_tagged_count.keys():
+            if best_tag[(word, tagg)] < words_tagged_count[(word, tagg)]:
+                taggs_for_words[word] = tagg
+                best_tag[(word, tagg)] = words_tagged_count[(word, tagg)]
+
+        self.taggs_for_words = taggs_for_words
 
     def tag(self, sent):
         """Tag a sentence.
@@ -17,14 +31,13 @@ class BaselineTagger:
 
     def tag_word(self, w):
         """Tag a word.
-
         w -- the word.
         """
-        return 'nc'
+        return self.taggs_for_words.get(w, 'nc0s000')
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        return w not in self.taggs_for_words
