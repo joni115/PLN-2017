28eab6ae1b316831e70ccb4e74abfd1d88646ecb
jonathanmutal <jonathanmutal95@gmail.com>
2017-06-11 01:43:03 -0300

    first attempt for upcfg

--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -1,3 +1,12 @@
+from nltk.tree import Tree
+
+from collections import defaultdict
+
+from parsing.util import lexicalize, unlexicalize
+
+from nltk.grammar import ProbabilisticProduction
+
+
 class UPCFG:
     """Unlexicalized PCFG.
     """
@@ -6,7 +15,19 @@ class UPCFG:
         """
         parsed_sents -- list of training trees.
         """
- 
+
+        count_X = defaultdict(lambda: defaultdict(int))
+        count_Y_Z = defaultdict(int)
+        for t in parsed_sents:
+            unle_tree = unlexicalize(t.copy(deep=True))
+            for unle_t in unle_tree:
+                unle_t.chomsky_normal_form()
+                for prod in unle_t.productions():
+                    count_gramat[prod.lhs()][prod.rhs()] += 1
+                    count_gramat[prod.lhs()] += 1
+
+
+
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
@@ -16,3 +37,13 @@ class UPCFG:
  
         tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
         """
+
+t = Tree.fromstring(
+    """
+        (S
+            (NP (Det el) (Noun gato))
+            (VP (Verb come) (NP (Noun pescado) (Adj crudo)))
+        )
+    """)
+
+model = UPCFG([t])
