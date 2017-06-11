41224c93663809fe127bc7fa88839d64adf96547
jonathanmutal <jonathanmutal95@gmail.com>
2017-06-11 09:57:14 -0300

    implementing init in upcfg

--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -16,21 +16,28 @@ class UPCFG:
         parsed_sents -- list of training trees.
         """
 
-        count_X = defaultdict(lambda: defaultdict(int))
-        count_Y_Z = defaultdict(int)
+        count_Y_Z = defaultdict(lambda: defaultdict(int))
+        count_X = defaultdict(int)
         for t in parsed_sents:
             unle_tree = unlexicalize(t.copy(deep=True))
             for unle_t in unle_tree:
                 unle_t.chomsky_normal_form()
                 for prod in unle_t.productions():
-                    count_gramat[prod.lhs()][prod.rhs()] += 1
-                    count_gramat[prod.lhs()] += 1
+                    count_Y_Z[prod.lhs()][prod.rhs()] += 1
+                    count_X[prod.lhs()] += 1
 
 
+        productions = []
+        for X, c_X in count_X.items():
+            for (Y_Z, c_Y_Z) in count_Y_Z[X].items():
+                productions.append(ProbabilisticProduction(X, Y_Z, probs=float(c_Y_Z) / float(c_X)))
+
+        self.productions = productions
 
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
+        return self.productions
  
     def parse(self, tagged_sent):
         """Parse a tagged sentence.
@@ -38,12 +45,12 @@ class UPCFG:
         tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
         """
 
-t = Tree.fromstring(
-    """
-        (S
-            (NP (Det el) (Noun gato))
-            (VP (Verb come) (NP (Noun pescado) (Adj crudo)))
-        )
-    """)
+# t = Tree.fromstring(
+#     """
+#         (S
+#             (NP (Det el) (Noun gato))
+#             (VP (Verb come) (NP (Noun pescado) (Adj crudo)))
+#         )
+#     """)
 
-model = UPCFG([t])
+# model = UPCFG([t])
