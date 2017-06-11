d786f01728b53762110a883e540b701a8c4f9d1e
jonathanmutal <jonathanmutal95@gmail.com>
2017-06-11 12:09:46 -0300

    adding parse implementation

--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -4,7 +4,9 @@ from collections import defaultdict
 
 from parsing.util import lexicalize, unlexicalize
 
-from nltk.grammar import ProbabilisticProduction
+from nltk.grammar import ProbabilisticProduction, PCFG, Nonterminal
+
+from parsing.cky_parser import CKYParser
 
 
 class UPCFG:
@@ -15,12 +17,13 @@ class UPCFG:
         """
         parsed_sents -- list of training trees.
         """
+        self.start = Nonterminal(start)
 
         count_Y_Z = defaultdict(lambda: defaultdict(int))
         count_X = defaultdict(int)
         for t in parsed_sents:
-            unle_tree = unlexicalize(t.copy(deep=True))
-            for unle_t in unle_tree:
+            unle_trees = unlexicalize(t.copy(deep=True))
+            for unle_t in unle_trees:
                 unle_t.chomsky_normal_form()
                 for prod in unle_t.productions():
                     count_Y_Z[prod.lhs()][prod.rhs()] += 1
@@ -30,27 +33,43 @@ class UPCFG:
         productions = []
         for X, c_X in count_X.items():
             for (Y_Z, c_Y_Z) in count_Y_Z[X].items():
-                productions.append(ProbabilisticProduction(X, Y_Z, probs=float(c_Y_Z) / float(c_X)))
+                q = c_Y_Z / float(c_X)
+                productions.append(ProbabilisticProduction(X, Y_Z, prob=q))
 
-        self.productions = productions
+        self.production = productions
+
+        grammar = PCFG(Nonterminal(start), productions)
+        self.parser = CKYParser(grammar)
 
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
-        return self.productions
+        return self.production
  
     def parse(self, tagged_sent):
         """Parse a tagged sentence.
  
         tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
         """
+        sent, tags = zip(*tagged_sent)
+        prob_sent, tree = self.parser.parse(tags)
 
-# t = Tree.fromstring(
-#     """
-#         (S
-#             (NP (Det el) (Noun gato))
-#             (VP (Verb come) (NP (Noun pescado) (Adj crudo)))
-#         )
-#     """)
+        if prob_sent == float('-inf'):
+            return Tree(self.start, [Tree(tag, [word]) for word, tag in tagged_sent])
 
-# model = UPCFG([t])
+        print('me dio diferente de -inf')
+        return lexicalize(tree, sent)
+
+t = Tree.fromstring(
+    """
+        (S
+            (NP (Det el) (Noun gato))
+            (VP (Verb come) (NP (Noun pescado) (Adj crudo)))
+        )
+    """)
+
+model = UPCFG([t])
+sent = 'gato el come pescado crudo'.split()
+tags = 'Noun Det Verb Noun Adj'.split()
+tagged_sent = list(zip(sent, tags))
+tree = model.parse(tagged_sent)
