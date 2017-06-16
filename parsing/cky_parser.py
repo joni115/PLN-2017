dc3f148d8b19330560bb254f1fe6938db68b7945
jonathanmutal <jonathanmutal95@gmail.com>
2017-06-16 16:32:13 -0300

    removing print

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -61,7 +61,6 @@ class CKYParser:
                     # have probabilty > 0 
                     for Y, y_prob in pi[(i, s)].items():
                         for Z, z_prob in pi[(s + 1, j)].items():
-                            print(self.__q_prob)
                             for X, x_prob in self.__q_prob.get((Y, Z), {}).items():
                                 prob = y_prob + z_prob + x_prob
                                 best_prob = pi[(i, j)].get(X, float('-inf'))
