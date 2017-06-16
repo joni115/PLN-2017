40581fca990912c658a305de3bc1eb5b1a615098
jonathanmutal <jonathanmutal95@gmail.com>
2017-06-16 16:31:26 -0300

    optimization for cky parser

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -15,17 +15,25 @@ class CKYParser:
         """
         grammar -- a binarised NLTK PCFG. In chomsky normal form.
         """
-        self.S = str(grammar.start())
+        self.__S = grammar.start().symbol()
         q = defaultdict(dict)
-        q_2 = defaultdict(dict)
 
+        # we want to distinguish nonTerminal from terminal for inicialization.
+        # would be util in parse.
         for prod in grammar.productions():
             if len(prod.rhs()) == 1:
-                q_2[prod.rhs()][prod.lhs()] = prod.prob()
-            q[str(prod.lhs())][prod.rhs()] = prod.prob()
+                # terminals
+                q[prod.rhs()][prod.lhs().symbol()] = prod.prob()
+            else:
+                # nonTerminals
+                # for passing test. Symbol is a string representation
+                # for nonTerminals.
+                # q will be a dictonary where keys are
+                # (nonTerminal, nonTerminal) and (terminals,).
+                rhs = (prod.rhs()[0].symbol(), prod.rhs()[1].symbol())
+                q[rhs][prod.lhs().symbol()] = prod.prob()
 
-        self.q_nonTerminals = dict(q)
-        self.q_terminals = dict(q_2)
+        self.__q_prob = dict(q)
 
     def parse(self, sent):
         """Parse a sequence of terminals.
@@ -33,36 +41,37 @@ class CKYParser:
         sent -- the sequence of terminals.
         """
         n = len(sent)
-        # set of Nonterminals
-        N = self.q_nonTerminals.keys()
 
         # inicialization
         pi = defaultdict(dict)
         bp = defaultdict(dict)
         for i in range(1, n + 1):
             xi = sent[i-1]
-            for nonT, prob in self.q_terminals[(xi,)].items():
-                pi[(i,i)][str(nonT)] = log2m(prob)
-                bp[(i,i)][str(nonT)] = Tree(str(nonT), [xi])
+            for nonT, prob in self.__q_prob[(xi,)].items():
+                pi[(i,i)][nonT] = log2m(prob)
+                bp[(i,i)][nonT] = Tree(nonT, [xi])
 
         for l in range(1, n):
+            # begin 1
             for i in range(1, n-l+1):
                 j = i + l
                 for s in range(i, j):
-                    for X in N:
-                        R = ((term, prob) for (term, prob) in self.q_nonTerminals[X].items() if len(term) == 2)
-                        for ((Y, Z), prob) in R:
-                            prob = log2m(prob)
-                            prob += pi[(i, s)].get(str(Y), float('-inf'))
-                            prob +=  pi[(s+1, j)].get(str(Z), float('-inf'))
-                            actual_prob = pi[(i, j)].get(X, float('-inf'))
-                            if prob > actual_prob:
-                               pi[(i, j)][X] = prob
-                               last_tree = [bp[(i, s)][str(Y)], bp[(s+1, j)][str(Z)]]
-                               bp[(i, j)][X] = Tree(X, last_tree)
+                    # optimization.
+                    # we only want the X-> Y Z in R for s={i..j} where X -> Y Z
+                    # have probabilty > 0 
+                    for Y, y_prob in pi[(i, s)].items():
+                        for Z, z_prob in pi[(s + 1, j)].items():
+                            print(self.__q_prob)
+                            for X, x_prob in self.__q_prob.get((Y, Z), {}).items():
+                                prob = y_prob + z_prob + x_prob
+                                best_prob = pi[(i, j)].get(X, float('-inf'))
+                                if best_prob < prob:
+                                    pi[(i, j)][X] = prob
+                                    last_tree = [bp[(i, s)][Y], bp[(s+1, j)][Z]]
+                                    bp[(i, j)][X] = Tree(X, last_tree)
 
-        best_prob = pi[(1, n)].get(self.S, float('-inf'))
-        best_parser = bp[(1, n)].get(self.S, None)
+        best_prob = pi[(1, n)].get(self.__S, float('-inf'))
+        best_parser = bp[(1, n)].get(self.__S, None)
 
         self._pi = dict(pi)
         self._bp = dict(bp)
